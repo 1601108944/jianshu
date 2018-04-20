113fa62f4f8d0faa9d19c3828460a0196bb59a19
zhangzhuron <1601108944@qq.com>
2018-04-20 11:52:28 +0800

    4.20 上午搜索框

--- components/myHeader.vue
+++ components/myHeader.vue
@@ -12,7 +12,7 @@
         </nuxt-link>
 
         <!-- 登陆用户信息 -->
-        <div class="user" @mouseover="isShow=true" @mouseout="isShow=false">
+        <div class="user" @mouseover="isShow=true" @mouseleave="isShow=false">
               <div class="drop-down">
                <nuxt-link to="/u123" class="avatar">
               <img src="../assets/img/default-avatar.jpg" alt="">
@@ -52,7 +52,64 @@
               </ul>
         </div>
         <!-- 右上角，登陆和注册 -->
+        <!-- 导航部分 -->
+        <div class="container">
+              <ul class="nav-list">
+                  <li>
+                       <nuxt-link to="/"  class="active">
+                          <i class="fa fa-compass"></i>
+                          <span>发现</span>
+                       </nuxt-link>
+                  </li>
+                  <li>
+                      <nuxt-link to="/follow"> 
+                          <i class="fa fa-book"></i>
+                          <span>关注</span>
+                      </nuxt-link>
+                  </li>
+                  <li class="user" @mousemove="userShow=true" @mouseout="userShow=false">
+                      <nuxt-link to="/notify">
+                          <i class="fa fa-bell-o"></i>
+                          <span>消息</span>
+                      </nuxt-link>
 
+                      <ul class="drop-menu"  v-show="userShow">
+                          <li>
+                              <nuxt-link to="/">
+                                  <i class="fa fa-comment-o"></i>
+                                  <span>评论</span>
+                              </nuxt-link>
+                          </li>
+                          <li>
+                              <nuxt-link to="/">
+                                  <i class="fa fa-envelope-o"></i>
+                                  <span>简信</span>
+                              </nuxt-link>
+                          </li>
+                          <li>
+                              <nuxt-link to="/">
+                                  <i class="fa fa-square-o"></i>
+                                  <span>关注</span>
+                              </nuxt-link>
+                          </li>
+                          <li>
+                              <nuxt-link to="/">
+                                  <i class="fa fa-heart-o"></i>
+                                  <span>喜欢和赞</span>
+                              </nuxt-link>
+                          </li>
+                      </ul>
+                  </li>
+                  <li class="search">
+                   <form method="post">
+                       <input type="text" placeholder="搜索" class="search-input">
+                       <a href="#" class="search-btn">
+                           <i class="fa fa-search"></i>
+                       </a>
+                   </form>
+                  </li>
+              </ul>
+        </div>
       </div>
       </nav>
   </div>
@@ -61,10 +118,11 @@
 <script>
 export default {
   name: "myHeader",
-  data(){
-      return{
-          isShow:false
-      }
+  data() {
+    return {
+      isShow: false,
+      userShow: false
+    };
   }
 };
 </script>
@@ -106,78 +164,189 @@ nav .logo img {
   height: 100%;
 }
 
-nav .write-btn{
-    float: right;
-    width: 100px;
-    height: 40px;
-    line-height: 40px;
-    margin: 8px 15px 0 ;
-    border-radius: 20px;
-    font-size: 15px;
-    color: #fff;
-    background: #ea6f5a;
-    text-align: center;
+nav .write-btn {
+  float: right;
+  width: 100px;
+  height: 40px;
+  line-height: 40px;
+  margin: 8px 15px 0;
+  border-radius: 20px;
+  font-size: 15px;
+  color: #fff;
+  background: #ea6f5a;
+  text-align: center;
 }
-nav .write-btn:hover{
-    background: #ec6149;
-}
-
-nav .user{
-    position: relative;
-    float: right;
+nav .write-btn:hover {
+  background: #ec6149;
 }
 
-nav .user .avatar{
-    position: relative;
-    display: block;
-    width: 40px;
-    height: 40px;
-    margin: 8px 28px 8px 20px;
+nav .user {
+  position: relative;
+  float: right;
 }
 
-nav .user:hover{
-    background: #f5f5f5;
+nav .user .avatar {
+  position: relative;
+  display: block;
+  width: 40px;
+  height: 40px;
+  margin: 8px 28px 8px 20px;
 }
-nav .user .avatar:before{
-    content:'';
-    position: absolute;
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    border-top: 5px solid #999;
-    top: 18px;
-    right: -14px;
+
+nav .user:hover {
+  background: #f5f5f5;
+}
+nav .user .avatar:before {
+  content: "";
+  position: absolute;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 5px solid #999;
+  top: 18px;
+  right: -14px;
 }
 nav .user .avatar img {
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    border:1px solid #eee;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: 1px solid #eee;
 }
-nav .user .drop-menu{
-    position: absolute;
-    box-shadow: 0 0 8px rgb(0, 0, 0,.1);
-    min-width: 160px;
-    z-index: 999;
-    margin: 0;
-    padding: 10px 0;
-    font-size: 15px;
+nav .user .drop-menu {
+  position: absolute;
+  box-shadow: 0 0 8px rgb(0, 0, 0, 0.1);
+  min-width: 160px;
+  z-index: 999;
+  margin: 0;
+  padding: 10px 0;
+  font-size: 15px;
 }
-nav .user .drop-menu li a{
-    padding:10px 20px ;
-    display: block;
-    line-height: 30px;
+nav .user .drop-menu li a {
+  padding: 10px 20px;
+  display: block;
+  line-height: 30px;
 }
-nav .user .drop-menu li a i{
-    margin-right: 12px;
-    color: #ea6f5a;
-    width: 20px;
-    height: 20px;
-    font-size: 20px;
-    display: inline-block;
+nav .user .drop-menu li a i {
+  margin-right: 12px;
+  color: #ea6f5a;
+  width: 20px;
+  height: 20px;
+  font-size: 20px;
+  display: inline-block;
 }
-nav .user .drop-menu li a:hover{
-    background: #f5f5f5;
+nav .user .drop-menu li a:hover {
+  background: #f5f5f5;
 }
 
+nav .nav-list {
+  float: left;
+  margin: 0;
+}
+/* nav .nav-list:after{
+     height: 0;
+     content: '';
+     display: block;
+     visibility: hidden;
+     clear: both;
+} */
+nav .nav-list > li {
+  float: left;
+  margin-right: 5px;
+}
+nav .nav-list > li a {
+  display: block;
+  height: 56px;
+  line-height: 26px;
+  padding: 15px;
+}
+nav .nav-list > li a.active {
+  color: #ea6f5a;
+  background: none !important;
+}
+nav .nav-list > li a:hover {
+  background: #f5f5f5;
+}
+nav .nav-list > li a i {
+  margin-right: 5px;
+  font-size: 20px;
+}
+
+nav .nav-list .search {
+  padding-left: 15px;
+  margin-right: 10px;
+}
+nav .nav-list .search form {
+  top: 9px;
+  margin-bottom: 20px;
+  position: relative;
+}
+nav .nav-list .search form .search-input {
+  width: 240px;
+  height: 38px;
+  font-size: 14px;
+  padding: 0 40px 0 20px;
+  border: 1px solid #eee;
+  background: #eee;
+  border-radius: 40px;
+  transition: width 0.5s;
+}
+nav .nav-list .search form .search-input:focus {
+  width: 320px;
+}
+nav .nav-list .search form .search-input:focus ~ .search-btn {
+  background: #969696;
+  color: white;
+  border-radius: 50%;
+  font-size: 15px;
+}
+@media (max-width: 1440px) {
+  nav .nav-list > li a i {
+    display: none;
+  }
+  nav .nav-list .search form .search-input {
+    width: 160px;
+  }
+  nav .nav-list .search form .search-input:focus {
+    width: 240px;
+  }
+}
+
+@media(max-width:1080px){
+
+    nav .nav-list li span{
+        display: none;
+    }
+    nav .nav-list>li a i{
+        display: block;
+    }
+  nav .nav-list .search form .search-input:focus {
+    width: 160px;
+  }
+}
+@media(max-width:768px){
+    nav .nav-list{
+        display: none;
+    }
+}
+
+nav .nav-list .search form .search-btn {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  top: 4px;
+  right: 5px;
+  color: #969696;
+  text-align: center;
+  padding: 0;
+}
+nav .nav-list .search form .search-btn:hover {
+  background: none;
+}
+
+nav .nav-list .search form .search-btn i {
+  display: block !important;
+  margin: 0;
+  text-align: center;
+  line-height: 30px;
+}
 </style>
 
